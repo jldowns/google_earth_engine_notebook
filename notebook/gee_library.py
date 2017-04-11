c93f4af7f0131a0da6f77c29a56309e2b020d640
jldowns <jldowns@users.noreply@github.com>
2017-04-10 20:26:19 -0700

    got rough image size calculator working

--- notebook/gee_library.py
+++ notebook/gee_library.py
@@ -5,21 +5,25 @@ import wget
 import datetime
 import zipfile
 import ee
+import matplotlib.image as mpimg
 
 
-def unzipURL(img_url):
-    """ Downloads and unzips a file. Returns the path where everything is unzipped. """
-    TMP_DIR="./tmp/"+str(random.randrange(999999))
-    zip_filename = os.path.join(TMP_DIR, 'zipped_tile.zip')
+def unzipURL(img_url, tmp_directory = None):
+    """
+    Downloads and unzips a file in a tmp directory. Returns the path where everything is unzipped.
+    """
+    if tmp_directory == None:
+        tmp_directory="./tmp/"+str(random.randrange(999999))
+    zip_filename = os.path.join(tmp_directory, 'zipped_tile.zip')
 
     # make tmp directory
-    if not os.path.exists(TMP_DIR):
-        os.makedirs(TMP_DIR)
+    if not os.path.exists(tmp_directory):
+        os.makedirs(tmp_directory)
 
     # and delete everything in it:
-    filelist = [ f for f in os.listdir(TMP_DIR)]
+    filelist = [ f for f in os.listdir(tmp_directory)]
     for f in filelist:
-        os.remove(os.path.join(TMP_DIR, f))
+        os.remove(os.path.join(tmp_directory, f))
 
 
     # try:
@@ -34,49 +38,11 @@ def unzipURL(img_url):
 
     wget.download(img_url, zip_filename)
     zip_ref = zipfile.ZipFile(zip_filename, 'r')
-    zip_ref.extractall(TMP_DIR)
+    zip_ref.extractall(tmp_directory)
     zip_ref.close()
 
-    return TMP_DIR
+    return tmp_directory
 
-    return
-
-# def element_to_img(geElement, resolution, bands, bounds):
-    """
-    Converts a google earth engine Element object (an element of an Image Collection)
-    into a numpy array that you can display.
-
-    bands: ['10', '20', '30']
-    """
-
-    DEFAULT_MAP_NAME = 'map_section'
-
-    # create the Google Earth Engine Image object
-    gee_img = ee.Image(geElement)
-
-    # convert the simple list of band names into the dictionary that the query needs
-    band_query_dict = [{'id': b} for b in bands]
-
-    # get the download link from google
-    path=gee_img.getDownloadUrl({
-            'name': DEFAULT_MAP_NAME,  # name the file (otherwise it will be a uninterpretable hash)
-            'scale': resolution,  # resolution in meters
-            'crs': 'EPSG:4326', #4326                         #  projection
-            'bands': band_query_dict,
-            'region': str(bounds.getInfo()['coordinates'])
-            });
-
-    # debug output
-    print path
-
-    # convert to tiff
-    tif_location = unzipURL(path)
-
-    # get handles to the tiff files
-    tif_filenames = [DEFAULT_MAP_NAME+'.'+b+'.tif' for b in bands]
-    tif_paths = [os.path.join(tif_location, tf) for tf in tif_filenames]
-
-    return tif_paths
 
 
 def image_size_at_resolution(bounds_geometry, resolution):
@@ -105,12 +71,13 @@ def image_size_at_resolution(bounds_geometry, resolution):
 
 
 
-
-
-
-
-
 def img_at_region(geCollection, resolution, bands, geo_bounds):
+    tif_band_dictionary = tif_at_region(geCollection, resolution, bands, geo_bounds)
+
+    return [mpimg.imread(t) for t in tif_band_dictionary.values()]
+
+
+def tif_at_region(geCollection, resolution, bands, geo_bounds):
     """
     Converts a google earth engine Element object (an element of an Image Collection)
     into a numpy array that you can display.
@@ -121,7 +88,7 @@ def img_at_region(geCollection, resolution, bands, geo_bounds):
     DEFAULT_MAP_NAME = 'map_section'
 
     # create the Google Earth Engine Image object
-    gee_img = geCollection.select(bands).mosaic()
+    gee_img = geCollection.filterBounds(geo_bounds).select(bands).mosaic()
 
     # convert the simple list of band names into the dictionary that the query needs
     bands_string = [{'id': b} for b in bands]
@@ -138,14 +105,20 @@ def img_at_region(geCollection, resolution, bands, geo_bounds):
     # debug output
     print path
 
-    # convert to tiff
+    # unzip the tiff
     tif_location = unzipURL(path)
 
-    # get handles to the tiff files
-    tif_filenames = [DEFAULT_MAP_NAME+'.'+b+'.tif' for b in bands]
-    tif_paths = [os.path.join(tif_location, tf) for tf in tif_filenames]
+    tif_filename_object = {}
+    for b in bands:
+        # each tif in the zip file is in the format 'mapname.band.tif'
+        tif_filename = DEFAULT_MAP_NAME+'.'+b+'.tif'
+        tif_path = os.path.join(tif_location, tif_filename)
+        # add the calculated tif path to a dictionary, keyed by the band name
+        tif_filename_object[b] = tif_path
 
-    return tif_paths
+
+
+    return tif_filename_object
 
 
 
@@ -177,13 +150,6 @@ def date_range(image_collection):
 def collection_length(image_collection):
     return image_collection.size().getInfo()
 
-def all_images_at_location(image_collection, coordinates):
-    """
-    coordinates: (lat, long)
-    """
-    point = ee.Geometry.Point(coordinates)
-    return image_collection.filterBounds(point)
-
 def bound_geometry(corner1, corner2):
     """ Given 2 points, returns a Geometry object that represents the square
     those 2 points make up. """
@@ -192,14 +158,3 @@ def bound_geometry(corner1, corner2):
     x2, y2 = corner2
 
     return ee.Algorithms.GeometryConstructors.LinearRing([[x1,y1],[x1,y2],[x2,y2],[x2,y1], [x1,y1]])
-
-
-def bound_string(corner1, corner2):
-    """
-    Returns a string representation of the square defined by the 2 corners.
-    """
-    ulx, uly = corner1
-    lrx, lry = corner2
-
-    region =  [ulx,lry], [ulx, uly], [lrx, uly], [lrx, lry]  #h11v08
-    return str(list(region))
